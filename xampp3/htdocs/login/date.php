d2b9b39c88a8089e4385c1bc76cf0f364bfe9c17
kawano <kawano@groxi.jp>
2023-07-06 17:38:08 +0900

    アンケート入力　DB接続

--- xampp3/htdocs/login/date.php
+++ xampp3/htdocs/login/date.php
@@ -13,16 +13,56 @@ if ($db->connect_error) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // フォームからのデータを取得
-  $gender = $_POST["gender"];
-  $blood = $_POST["blood"];
-  $opinion = $_POST["opinion"];
+  $gender = isset($_POST["gender"]) ? $_POST["gender"] : '';
+  $blood = isset($_POST["blood"]) ? $_POST["blood"] : '';
+  $opinion = isset($_POST["opinion"]) ? $_POST["opinion"] : '';
 
   // SQLクエリの作成と実行
-  $sql = "INSERT INTO  users(gender, blood, opinion) VALUES ('$gender', '$blood', '$opinion')";
+  $sql = "INSERT INTO  form1(gender, blood, opinion) VALUES ('$gender', '$blood', '$opinion')";
   if ($db->query($sql) === TRUE) {
-      echo "送信完了しました。";
+      echo '<p class="message">送信完了しました。</p>';
+
   } else {
       echo "上手く送信できませんでした。: " . $sql . "<br>" . $db->error;
   }
 }
 ?>
+
+
+
+<?php
+
+session_start();
+if (!isset($_SESSION['username'])) {
+  header("Location: login.php");
+  exit();
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="date.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+</head>
+
+<body>
+<div class="button-container">
+      <form action="home.php">
+        <input type="submit" value="HOME画面へ" />
+      </form>
+      <br />
+      <form action="logout.php" method="post">
+        <input type="hidden" name="redirect" value="login.php" />
+        <input type="submit" value="ログアウト" />
+      </form>
+      
+    </div>
+  
+</body>
+</html>
